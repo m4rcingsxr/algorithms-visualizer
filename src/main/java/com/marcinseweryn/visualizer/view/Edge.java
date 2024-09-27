d7e6d71105f612cafbf651476bb52a890b20affa
Marcin Seweryn <marcinsewerynn@gmail.com>
2024-09-27 20:00:09 +0200

    add distance label to display calculated Euclidean distance between two nodes

--- src/main/java/com/marcinseweryn/visualizer/view/Edge.java
+++ src/main/java/com/marcinseweryn/visualizer/view/Edge.java
@@ -2,7 +2,9 @@ package com.marcinseweryn.visualizer.view;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
@@ -14,9 +16,12 @@ public class Edge extends PaneArrow {
     private final DoubleProperty weight = new SimpleDoubleProperty();
 
     private final Label weightLabel = new Label();
+    private final Label distanceLabel = new Label();
 
     public Edge(GraphNode vertex1, GraphNode vertex2) {
-        super(vertex1.getLayoutX(), vertex1.getLayoutY(), vertex2.getLayoutX(), vertex2.getLayoutY());
+        super(vertex1.getLayoutX(), vertex1.getLayoutY(), vertex2.getLayoutX(),
+              vertex2.getLayoutY()
+        );
 
         this.vertex1 = vertex1;
         this.vertex2 = vertex2;
@@ -27,17 +32,32 @@ public class Edge extends PaneArrow {
         this.y2.bind(vertex2.layoutYProperty());
 
         this.weight.addListener((obs, oldVal, newVal) -> {
-           if (newVal.doubleValue() != 0) {
-               mainLine.getStyleClass().add("weighted");
-           } else {
-               mainLine.getStyleClass().remove("weighted");
-           }
+            if (newVal.doubleValue() != 0) {
+                mainLine.getStyleClass().add("weighted");
+            } else {
+                mainLine.getStyleClass().remove("weighted");
+            }
         });
 
         this.weightLabel.visibleProperty().bind(this.weight.isNotEqualTo(0));
         this.weightLabel.textProperty().bind(Bindings.concat("w:[", this.weight.asString(), "]"));
 
-        addContent(new HBox(5, weightLabel));
+        IntegerProperty distance = new SimpleIntegerProperty();
+        distance.bind(Bindings.createDoubleBinding(() ->
+                                                           Math.sqrt(
+                                                                   Math.pow(getY2() - getY1(), 2) +
+                                                                           Math.pow(
+                                                                                   getX2() - getX1(),
+                                                                                   2
+                                                                           )
+                                                           ),
+                                                   y2Property(), y1Property(), x2Property(),
+                                                   x1Property()
+                      )
+        );
+        this.distanceLabel.textProperty().bind(Bindings.concat("d:[", distance.asString(), "]"));
+
+        addContent(new HBox(5, weightLabel, distanceLabel));
     }
 
     public GraphNode getNeighbour(GraphNode vertex) {
@@ -45,7 +65,7 @@ public class Edge extends PaneArrow {
     }
 
     public void setHeadAVisible(GraphNode vertex, boolean b) {
-        if(vertex == vertex1) {
+        if (vertex == vertex1) {
             setHeadAVisible(b);
         } else {
             setHeadBVisible(b);
